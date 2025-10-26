47bf1c2117e7d977665e75f49b6603dc776968a5
Minh Đức <118919625+Haveroahl@users.noreply.github.com>
2025-10-26 20:14:51 +0700

    Add main.py to run RPS game simulations

---
+++ main.py
@@ -0,0 +1,7 @@
+from RPS_game import play, quincy, abbey, kris, mrugesh
+from RPS import player
+
+print(play(player, quincy, 1000))
+print(play(player, abbey, 1000))
+print(play(player, kris, 1000))
+print(play(player, mrugesh, 1000))
